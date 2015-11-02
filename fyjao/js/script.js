f62e63d59e5d5df4153ccc8ff890bdb0f193437e
Faye Jao <yjao@uci.edu>
2015-11-02 13:47:29 -0800

    no message

--- fyjao/js/script.js
+++ fyjao/js/script.js
@@ -10,7 +10,7 @@ $(document).ready(
     function()
     {
       $('#header').load("header.html");
-			$(body).css("paddingTop", $('#header').height());
+			$('body').css("paddingTop", $('#header').height());
 			alert("changed!");
     }
   );
