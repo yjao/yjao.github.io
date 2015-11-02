4c68c0b9c89400614282ec197d5023cd99ad86bd
Faye Jao <yjao@uci.edu>
2015-11-02 00:19:25 -0800

    initial push

---
+++ fyjao/js/script.js
@@ -0,0 +1,15 @@
+/*$(document).ready(
+    function()
+    {
+      $("#portrait").fadeOut(1000);
+    }
+  );
+  */
+  
+$(document).ready(
+    function()
+    {
+      $('#header').load("header.html");
+    }
+  );
+
