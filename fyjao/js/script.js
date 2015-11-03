85a10208265956b802211fc632c671b573fb1e11
Faye Jao <yjao@uci.edu>
2015-11-03 13:14:31 -0800

    no message

--- fyjao/js/script.js
+++ fyjao/js/script.js
@@ -9,16 +9,16 @@
 $(document).ready(
     function()
     {
-      alert("1:12");
+      alert("1:14");
 			$('#header').load("header.html");
-			$('#header').load(
+			
+    }
+  );
+
+$('#header').load(
 				function()
 				{
 					$('body').css("paddingTop", $('#header').height());
 					alert($('#header').height());
 				}
 			);
-    }
-  );
-
-
