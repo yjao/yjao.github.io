e02ae14728fd411fdb01ed72abe4dfdaef8f4a60
Faye Jao <yjao@uci.edu>
2016-01-09 17:43:00 -0800

    no message

--- ideabox/scripts/script.js
+++ ideabox/scripts/script.js
@@ -2,7 +2,7 @@ var Submit = function()
 {
   var data = new FormData();
   data.append("data" , $("#contents").val());
-  var xhr = new XMLHttpRequest();
+  var xhr = (window.XMLHttpRequest) ? new XMLHttpRequest() : new activeXObject("Microsoft.XMLHTTP");
   xhr.open( 'post', 'script.php', true );
   xhr.send(data);
 }
